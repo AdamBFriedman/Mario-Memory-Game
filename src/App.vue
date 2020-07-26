8f6902466607181d217ef9e7655bb95539c0d4b7
Adam Friedman <adam@blok.party>
2020-07-25 19:14:56 -0700

    Created cardsArray variable

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    Hello there chaps
+    <div id="game"></div>
     <!-- <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>-->
   </div>
@@ -8,7 +8,58 @@
 
 <script>
 // import HelloWorld from "./components/HelloWorld.vue";
+let cardsArray = [
+  {
+    name: "shell",
+    img: "img/blueshell.png",
+  },
+  {
+    name: "star",
+    img: "img/star.png",
+  },
+  {
+    name: "bobomb",
+    img: "img/bobomb.png",
+  },
+  {
+    name: "mario",
+    img: "img/mario.png",
+  },
+  {
+    name: "luigi",
+    img: "img/luigi.png",
+  },
+  {
+    name: "peach",
+    img: "img/peach.png",
+  },
+  {
+    name: "1up",
+    img: "img/1up.png",
+  },
+  {
+    name: "mushroom",
+    img: "img/mushroom.png",
+  },
+  {
+    name: "thwomp",
+    img: "img/thwomp.png",
+  },
+  {
+    name: "bulletbill",
+    img: "img/bulletbill.png",
+  },
+  {
+    name: "coin",
+    img: "img/coin.png",
+  },
+  {
+    name: "goomba",
+    img: "img/goomba.png",
+  },
+];
 
+console.log(cardsArray);
 export default {
   name: "App",
   components: {
